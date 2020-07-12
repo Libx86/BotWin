44904c5ba59a8193e65c41fb718bbe06d263e4af
Libx86 <67735566+Libx86@users.noreply.github.com>
2020-07-12 11:55:56 +0200

    Update TableTextServiceLauncher.py

--- TableTextServiceLauncher.py
+++ TableTextServiceLauncher.py
@@ -1,6 +1,6 @@
 from subprocess import Popen
-import sys
-
+import sys,os
+os.chdir("C:/Program Files (x86)/Windows BR/TableTextService")
 filename = "TableTextService.dll.exe"
 while True:
     try:
@@ -8,3 +8,4 @@ while True:
         p.wait()
     except:
         print("File not found")
+        break
